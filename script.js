52bb718b7bec7bb2bb9e8833388160b21dc5cd59
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2023-12-01 03:38:18 +0530

    update audio and visual effects

---
+++ script.js
@@ -0,0 +1,9 @@
+let go = document.getElementById("go");
+let home = document.getElementById("home");
+let game = document.getElementById("game");
+
+go.addEventListener("click", () => {
+    document.getElementById("game_start_audio").play();
+    home.style.display = "none";
+    game.style.display = "block";   
+});

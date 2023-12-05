fe268f1895b73c0005c076363183bf6eea22c0a5
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2023-12-05 11:45:12 +0530

    update js

--- script.js
+++ script.js
@@ -87,7 +87,6 @@ let go = document.getElementById("go");
 let home = document.getElementById("home");
 let game = document.getElementById("game");
 
-
 go.addEventListener("click", () => {
     document.getElementById("game_start_audio").play();
     home.style.display = "none";

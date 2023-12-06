dbcc704a6dfbccf0c4995bb47b717245a1a117e0
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2023-12-06 18:15:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -55,6 +55,7 @@ var response = function(txtGuess) {
     while (guessesTaken < document.getElementById("rounds").value && gameDone == 0) {
       if (txtGuess == correctGuess) {
         answer = "Congratulations, you scored " + (100 - guessesTaken) + " points  ";
+break;
       } else if (txtGuess < correctGuess && txtGuess > 0) {
         answer = "Too less, try a bigger value ";
       } else if (txtGuess > correctGuess && txtGuess < 101) {
